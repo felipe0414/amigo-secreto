8377387a09aeb24a6452f84a422f19ed4dc9a24a
fcelisa <felipe.celis@gmail.com>
2025-02-05 23:19:06 -0300

    crea lista de participantes

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// crear lista de participantes
+let participantes=[];
+// agrgar a amigos mediante la caja y el boton añadir
