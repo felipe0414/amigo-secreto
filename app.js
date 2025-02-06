3c18f53cc98bc70b092d479e3533b7f186c21465
fcelisa <felipe.celis@gmail.com>
2025-02-05 23:58:36 -0300

    creacion de funciones

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-// crear lista de participantes
+
 let participantes=[];
 // agrgar a amigos mediante la caja y el boton añadir
+function amigos() {
+    let nombreDigitado = document.getElementById ('amigo');
+    if (nombreDigitado == '') {
+        alert ('inserte un nombre!');
+    } else { participates.push = nombreDigitado;
+        }
+}
+function limpiarCaja() {
+    document.getElementById('amigo').value = '';
+    return;    
+}
+limpiarCaja();
+amigos ();
